a49158b508b50314e6794e755b7ce482ee02dac1
navneet-chugh <navneetchugh15@gmail.com>
2023-08-23 21:14:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
+document.addEventListener("DOMContentLoaded", function() {
+  const newTodoInput = document.getElementById("newTodoInput");
+  const addTodoBtn = document.getElementById("addTodoBtn");
+  const todoList = document.getElementById("todoList");
+
+  addTodoBtn.addEventListener("click", function() {
+    const todoText = newTodoInput.value.trim();
+
+    if (todoText !== "") {
+      const todoItem = document.createElement("li");
+      todoItem.textContent = todoText;
+      todoList.appendChild(todoItem);
+
+      newTodoInput.value = ""; // Clear the input
+    }
+  });
+});
 
